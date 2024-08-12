7cc691b37edad312fec3bc48484146261ce32c95
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-08-12 23:09:55 +0300

    Create script.sql

---
+++ StudentList/src/main/resources/script.sql
@@ -0,0 +1,9 @@
+create database crud;
+    use crud;
+create table students(
+    id int auto_increment primary key,
+    FirstName varchar(20) not null,
+    LastName varchar(20) not null,
+    COURSE  varchar(20) not null );
+
+insert into students(FirstName, LastName, COURSE) values("James","Colem","Java");
