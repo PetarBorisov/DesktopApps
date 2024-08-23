b2a00fd8e47a027dfe13a9d90ac737bd860810a9
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-08-23 23:55:06 +0300

    Update FlowerShopManagementsSystem.java

--- Flower_Shop/src/main/java/com/example/flower_shop/FlowerShopManagementsSystem.java
+++ Flower_Shop/src/main/java/com/example/flower_shop/FlowerShopManagementsSystem.java
@@ -4,17 +4,43 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class FlowerShopManagementsSystem extends Application {
+
+    private double x = 0;
+    private double y = 0;
+
+
     @Override
     public void start(Stage stage) throws IOException {
-       Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/hello-view.fxml")));
 
+       Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/hello-view.fxml")));
         Scene scene = new Scene(root);
+
+        root.setOnMousePressed((MouseEvent event) -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged((MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+
+            stage.setOpacity(.8);
+        });
+
+        root.setOnMouseReleased((MouseEvent event) -> {
+            stage.setOpacity(1);
+        });
+
+        stage.initStyle(StageStyle.TRANSPARENT );
+
         stage.setScene(scene);
         stage.show();
     }
