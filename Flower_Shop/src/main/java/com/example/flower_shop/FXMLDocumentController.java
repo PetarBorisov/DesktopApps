9cd5594f93f5392e17d2a4496a50b4975ec1d55c
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-08-24 15:02:50 +0300

    Update FXMLDocumentController.java

--- Flower_Shop/src/main/java/com/example/flower_shop/FXMLDocumentController.java
+++ Flower_Shop/src/main/java/com/example/flower_shop/FXMLDocumentController.java
@@ -1,16 +1,119 @@
 package com.example.flower_shop;
 
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class FXMLDocumentController implements Initializable {
 
+    @FXML
+    private Button close;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    public void login() {
+
+
+        String sql = "SELECT * FROM admins WHERE username = ? and password = ?";
+        connect = Database.connectDb();
+
+        try {
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, username.getText());
+            prepare.setString(2, password.getText());
+
+            result = prepare.executeQuery();
+
+            Alert alert;
+
+            if (username.getText().isEmpty() || password.getText().isEmpty()) {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            } else {
+                if (result.next()) {
+
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+
+                    // Затваряне на текущия прозорец и отваряне на новия
+                    Stage currentStage = (Stage) loginBtn.getScene().getWindow();
+                    currentStage.close();
+
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/dashboard.fxml"));
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                } else {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        System.exit(0);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        loginBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                login();
+            }
+        });
     }
+
+
 }
