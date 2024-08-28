ac52b082eb8326485d58f2f2dfb5606467e71266
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-08-28 22:18:23 +0300

    Update module-info.java

--- Flower_Shop/src/main/java/module-info.java
+++ Flower_Shop/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.flower_shop {
     requires java.sql;
 
     requires com.dlsc.formsfx;
+    requires mysql.connector.j;
 
     opens com.example.flower_shop to javafx.fxml;
     exports com.example.flower_shop;
