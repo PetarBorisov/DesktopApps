c6d935b3993208e9c2bd4dc00581a1cb3c6728bc
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-09-05 21:09:19 +0300

    Create dashboardController.java

---
+++ Flower_Shop/src/main/java/com/example/flower_shop/controllers/dashboardController.java
@@ -0,0 +1,143 @@
+package com.example.flower_shop.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class dashboardController implements Initializable {
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private Button availableFlowers_btn;
+
+    @FXML
+    private AnchorPane availableFlowers_form;
+
+    @FXML
+    private Button aviailableFlowers_addBtn;
+
+    @FXML
+    private Button aviailableFlowers_clearBtn;
+
+    @FXML
+    private TableColumn<?, ?> aviailableFlowers_col_flowerId;
+
+    @FXML
+    private TableColumn<?, ?> aviailableFlowers_col_flowerName;
+
+    @FXML
+    private TableColumn<?, ?> aviailableFlowers_col_price;
+
+    @FXML
+    private TableColumn<?, ?> aviailableFlowers_col_status;
+
+    @FXML
+    private Button aviailableFlowers_deleteBtn;
+
+    @FXML
+    private TextField aviailableFlowers_flowerID;
+
+    @FXML
+    private TextField aviailableFlowers_flowerName;
+
+    @FXML
+    private ImageView aviailableFlowers_imageView;
+
+    @FXML
+    private Button aviailableFlowers_importBtn;
+
+    @FXML
+    private TextField aviailableFlowers_price;
+
+    @FXML
+    private TextField aviailableFlowers_search;
+
+    @FXML
+    private ComboBox<?> aviailableFlowers_status;
+
+    @FXML
+    private TableView<?> aviailableFlowers_tableView;
+
+    @FXML
+    private Button aviailableFlowers_updateBtn;
+
+    @FXML
+    private Button close;
+
+    @FXML
+    private Label home_availableFlowers;
+
+    @FXML
+    private Button home_btn;
+
+    @FXML
+    private BarChart<?, ?> home_chart;
+
+    @FXML
+    private AnchorPane home_form;
+
+    @FXML
+    private Label home_totalCustomers;
+
+    @FXML
+    private Label home_totalIncome;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button minimize;
+
+    @FXML
+    private Button purchase_btn;
+
+    @FXML
+    private TableColumn<?, ?> purchase_col_flowerID;
+
+    @FXML
+    private TableColumn<?, ?> purchase_col_flowerName;
+
+    @FXML
+    private TableColumn<?, ?> purchase_col_price;
+
+    @FXML
+    private TableColumn<?, ?> purchase_col_quantity;
+
+    @FXML
+    private ComboBox<?> purchase_flowerID;
+
+    @FXML
+    private ComboBox<?> purchase_flowerName;
+
+    @FXML
+    private AnchorPane purchase_form;
+
+    @FXML
+    private Button purchase_payBtn;
+
+    @FXML
+    private Spinner<?> purchase_quantity;
+
+    @FXML
+    private TableView<?> purchase_tableView;
+
+    @FXML
+    private Label purchase_total;
+
+    @FXML
+    private Text username;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
