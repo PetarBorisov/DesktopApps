070fe4c031581df2e273e750bcbecbf0f3e64b53
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-09-29 13:31:41 +0300

    Update script.sql

--- Flower_Shop/src/main/resources/script.sql
+++ Flower_Shop/src/main/resources/script.sql
@@ -8,3 +8,13 @@ create table admins(
 );
 
 insert into admins(Username, Password) values('James', '11111');
+
+use flower;
+create table flowers(
+    flowerId int auto_increment primary key,
+    name varchar(100) not null,
+    status varchar(100) not null ,
+    price double,
+    date date,
+    image varchar(100)
+);
