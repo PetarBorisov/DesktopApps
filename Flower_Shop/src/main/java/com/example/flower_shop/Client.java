bec7fe00391a2b3d0c64c2cc94eb6e22c4506ce8
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-10-14 23:28:55 +0300

    Create Client.java

---
+++ Flower_Shop/src/main/java/com/example/flower_shop/Client.java
@@ -0,0 +1,58 @@
+package com.example.flower_shop;
+
+public class Client {
+
+    private Integer id;
+    private String firstName;
+    private String fathersName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Client(Integer id, String firstName, String fathersName, String lastName, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.fathersName = fathersName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public void setFathersName(String fathersName) {
+        this.fathersName = fathersName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
