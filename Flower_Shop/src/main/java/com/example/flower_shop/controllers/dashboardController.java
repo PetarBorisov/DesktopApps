aa0917c2022b408d9e1e46eafe146f37e147484d
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-12-12 20:53:00 +0200

    Translate

--- Flower_Shop/src/main/java/com/example/flower_shop/controllers/dashboardController.java
+++ Flower_Shop/src/main/java/com/example/flower_shop/controllers/dashboardController.java
@@ -367,7 +367,7 @@ public dashboardController(){}
                 alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error Message");
                 alert.setHeaderText(null);
-                alert.setContentText("Please fill all blank fields");
+                alert.setContentText("Моля попълнете всички полета!");
                 alert.showAndWait();
             }else {
                 // Check if the Flower ID is already EXIST
