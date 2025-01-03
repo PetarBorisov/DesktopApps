ba7823e8a4498415f0239de8addb99c9d43f33a0
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2025-01-04 00:41:40 +0200

    Create runing.bat

---
+++ Flower_Shop/out/artifacts/Flower_Shop_jar/runing.bat
@@ -0,0 +1,3 @@
+@echo off
+java --module-path "C:\Program Files\Java\JavaFx\javafx-sdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar "C:\Users\Petyr Borisov\Desktop\Flower_Shop\out\artifacts\Flower_Shop_jar\Flower_Shop.jar"
+pause
