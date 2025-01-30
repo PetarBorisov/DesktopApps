9093b351eb27154a9130e9ff10234adf5dfd412b
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2025-01-30 21:39:58 +0200

    Update runing.bat

--- Flower_Shop/out/artifacts/Flower_Shop_jar/runing.bat
+++ Flower_Shop/out/artifacts/Flower_Shop_jar/runing.bat
@@ -1,3 +1,2 @@
 @echo off
-java --module-path "C:\Program Files\Java\JavaFx\javafx-sdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar "C:\Users\Petyr Borisov\Desktop\Flower_Shop\out\artifacts\Flower_Shop_jar\Flower_Shop.jar"
-pause
+"C:\Program Files\Java\jdk-22\bin\javaw.exe" --module-path "C:\Program Files\Java\JavaFx\javafx-sdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar "C:\Users\Petyr Borisov\Desktop\Flower_Shop\out\artifacts\Flower_Shop_jar\Flower_Shop.jar"
